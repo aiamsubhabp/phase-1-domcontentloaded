4bd9cc4f0f2d2eef8d995cf6f261a5f42f92a6d2
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-03-26 14:41:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+function updateText(){
+    let text = document.getElementById('text');
+    text.textContent = 'This is really cool!'
+}
+//updateText()
+
+document.addEventListener("DOMContentLoaded", updateText)
